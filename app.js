9e5d37c1f7ab6bd4ba9e0d3366f1cdfb8fbdf6bd
asterchang96 <asterchang96@gmail.com>
2021-07-04 11:03:05 +0800

    動態引入餐廳資訊

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Include express from node_modules
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
+const restaurant_list = require('./restaurant.json')
 
+
+const app = express()
 const port = 3000
 
 // setting template engine
@@ -12,8 +14,10 @@ app.set('view engine', 'handlebars')
 //setting static files
 app.use(express.static('public'))
 
+//引入餐廳動態資料
 app.get('/', (req, res ) => {
-  res.render('index')
+  console.log(restaurant_list.results)
+  res.render('index', {restaurants : restaurant_list.results})
 })
 
 app.listen(port, () => {
