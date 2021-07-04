10dd06b91a36b606a44f27dee2a63489c5e6f242
asterchang96 <asterchang96@gmail.com>
2021-07-04 22:39:36 +0800

    更改參數名稱

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars')
 const restaurant_list = require('./restaurant.json')
 let pre_category_restaurant = new Set(); //所有餐廳類型
 let category_restaurant = [] //隨機選取後之餐廳類型
-let search_result = false
+let search_result_howmany_restaurants = false
 
 const app = express()
 const port = 3000
@@ -18,15 +18,11 @@ app.use(express.static('public'))
 
 //引入餐廳動態資料
 app.get('/', (req, res) => {
-  search_result = false
-
-  //search category
-  restaurant_list.results.forEach((restaurant) => {
-    pre_category_restaurant.add(restaurant.category)
-  })
+  search_result_howmany_restaurants = false
+  //隨機產生category
   category_restaurant = sortAndPick(pre_category_restaurant)
 
-  res.render('index', {restaurants : restaurant_list.results,search_result : search_result, category : category_restaurant})
+  res.render('index', {restaurants : restaurant_list.results,search_result : search_result_howmany_restaurants, category : category_restaurant})
 })
 
 //動態引入餐廳詳細資料
@@ -37,25 +33,24 @@ app.get('/restaurants/:restaurant', (req, res) => {
 
 //搜尋餐廳
 app.get('/search', (req, res) => {
-
+  category_restaurant = sortAndPick(pre_category_restaurant)
   const keyword = (req.query.keyword).replace(/\s*/g,"")
-
- //特殊keywords搜尋(未輸入)==>顯示全部資料
-  if(keyword === ''){
-    search_result = false
-  }
 
   //一般keywords搜尋、keywords分類搜尋==>顯示搜尋資料
   const restaurants = restaurant_list.results.filter((restaurant) =>{
     //搜尋到幾筆相關資料(搜尋後才產生)
-    search_result = true
+    search_result_howmany_restaurants = true
     //temp_restaurants 暫放搜尋資料
     let temp_restaurants = restaurant.name.toLowerCase().includes(keyword.toLowerCase())
     temp_restaurants += restaurant.category.includes(keyword)
     return temp_restaurants
   })
 
-  res.render('index', {restaurants : restaurants, keyword : keyword,search_result : search_result, category : category_restaurant})
+  //特殊keywords搜尋(未輸入)==>顯示全部資料
+  if(keyword === ''){
+    search_result_howmany_restaurants = false
+  }
+  res.render('index', {restaurants : restaurants, keyword : keyword,search_result : search_result_howmany_restaurants, category : category_restaurant})
 })
 
 app.listen(port, () => {
@@ -67,7 +62,10 @@ app.listen(port, () => {
 
 //隨機抽取4個
 function sortAndPick(pre_category_restaurant){
-
+  //search category
+  restaurant_list.results.forEach((restaurant) => {
+    pre_category_restaurant.add(restaurant.category)
+  })
 
   //set 改 arr
   let category_restaurant_list = [...pre_category_restaurant]
