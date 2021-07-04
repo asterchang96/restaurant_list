b4edeffc11cbe55f9385e48d1ddf817277825c2c
asterchang96 <asterchang96@gmail.com>
2021-07-04 15:34:11 +0800

    搜尋動態顯示

--- app.js
+++ app.js
@@ -15,15 +15,25 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 //引入餐廳動態資料
-app.get('/', (req, res ) => {
+app.get('/', (req, res) => {
   res.render('index', {restaurants : restaurant_list.results})
 })
 
 //動態引入餐廳詳細資料
-app.get('/restaurants/:restaurant', (req, res ) => {
+app.get('/restaurants/:restaurant', (req, res) => {
   //console.log(req.params.restaurant)
   const restaurant_id = restaurant_list.results.find((restaurant) => restaurant.id.toString() === req.params.restaurant.toString())
   res.render('show', {restaurant : restaurant_id})
+})
+
+//搜尋餐廳
+app.get('/search', (req, res) => {
+  console.log(req.query.keyword)
+  const keyword = req.query.keyword
+  const restaurants = restaurant_list.results.filter((restaurant) =>{
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', {restaurants : restaurants, keyword : keyword})
 })
 
 app.listen(port, () => {
