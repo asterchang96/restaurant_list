2915fcf4759f5ec798246bfd214b26fb5ca8977d
asterchang96 <asterchang96@gmail.com>
2021-07-04 22:29:40 +0800

    將搜尋searchbar優化(category移入keywords中)

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const restaurant_list = require('./restaurant.json')
-let category_restaurant = [] //餐廳類型
+let pre_category_restaurant = new Set(); //所有餐廳類型
+let category_restaurant = [] //隨機選取後之餐廳類型
+let search_result = false
 
 const app = express()
 const port = 3000
@@ -16,8 +18,7 @@ app.use(express.static('public'))
 
 //引入餐廳動態資料
 app.get('/', (req, res) => {
-  let search_result = false
-  let pre_category_restaurant = new Set();
+  search_result = false
 
   //search category
   restaurant_list.results.forEach((restaurant) => {
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
   })
   category_restaurant = sortAndPick(pre_category_restaurant)
 
-  res.render('index', {restaurants : restaurant_list.results,search_result : search_result,category:category_restaurant})
+  res.render('index', {restaurants : restaurant_list.results,search_result : search_result, category : category_restaurant})
 })
 
 //動態引入餐廳詳細資料
@@ -36,38 +37,25 @@ app.get('/restaurants/:restaurant', (req, res) => {
 
 //搜尋餐廳
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const category = req.query.category
-  console.log(req.query)
 
-  //搜尋到幾筆相關資料(搜尋後才產生)
-  search_result = true
+  const keyword = (req.query.keyword).replace(/\s*/g,"")
 
-  //searchbar 有資料
-  let searchbarTorestaurants = []
-  let categoryTorestaurants = []
-  if(!category){
-    //一般keywords搜尋
-    searchbarTorestaurants = restaurant_list.results.filter((restaurant) =>{
-      return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-    })
-    //keywords分類搜尋
-    searchbarTorestaurants = restaurant_list.results.filter((restaurant) =>{
-      return restaurant.category.includes(keyword)
-    })   
-  }else{
-    //category-group 搜尋...
-    categoryTorestaurants = restaurant_list.results.filter((restaurant) =>{
-      return restaurant.category.includes(category)
-    })    
+ //特殊keywords搜尋(未輸入)==>顯示全部資料
+  if(keyword === ''){
+    search_result = false
   }
 
+  //一般keywords搜尋、keywords分類搜尋==>顯示搜尋資料
+  const restaurants = restaurant_list.results.filter((restaurant) =>{
+    //搜尋到幾筆相關資料(搜尋後才產生)
+    search_result = true
+    //temp_restaurants 暫放搜尋資料
+    let temp_restaurants = restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    temp_restaurants += restaurant.category.includes(keyword)
+    return temp_restaurants
+  })
 
-
-  
-  const restaurants = searchbarTorestaurants.concat(categoryTorestaurants)
-
-  res.render('index', {restaurants : restaurants, keyword : keyword,search_result:search_result})
+  res.render('index', {restaurants : restaurants, keyword : keyword,search_result : search_result, category : category_restaurant})
 })
 
 app.listen(port, () => {
@@ -76,8 +64,11 @@ app.listen(port, () => {
 
 
 //function 區
-//隨機抽取5個
+
+//隨機抽取4個
 function sortAndPick(pre_category_restaurant){
+
+
   //set 改 arr
   let category_restaurant_list = [...pre_category_restaurant]
 
@@ -94,6 +85,6 @@ function sortAndPick(pre_category_restaurant){
     return array;
   }
   
-  //取5個
-  return shuffle(category_restaurant_list).slice(0,3)
+  //取4個
+  return shuffle(category_restaurant_list).slice(0,4)
 }
