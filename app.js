1bfa4e1be6c347ed80ac60c8fda79636355d684b
asterchang96@gmail.com <asterchang96@gmail.com>
2021-07-11 21:01:40 +0800

    feat: add restaurant seeder, and show detail

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
-const restaurant_list = require('./restaurant.json')
+const Restaurant_list = require('./models/restaurants')
 let pre_category_restaurant = new Set(); //所有餐廳類型
 let category_restaurant = [] //隨機選取後之餐廳類型
 let search_result_howmany_restaurants = false
@@ -31,42 +31,53 @@ db.once('open', () => {
 })
 
 
-
-
 //引入餐廳動態資料
 app.get('/', (req, res) => {
   search_result_howmany_restaurants = false
-  //隨機產生category
-  category_restaurant = sortAndPick(pre_category_restaurant)
 
-  res.render('index', {restaurants : restaurant_list.results,search_result : search_result_howmany_restaurants, category : category_restaurant})
+  // TODO 隨機產生category
+  //category_restaurant = sortAndPick(pre_category_restaurant)
+
+  //引入restaurant database
+  Restaurant_list.find()
+    .lean()
+    .then(restaurants => res.render('index', {restaurants , search_result : search_result_howmany_restaurants}))
+
+
 })
 
 //動態引入餐廳詳細資料
 app.get('/restaurants/:restaurant', (req, res) => {
-  const restaurant_id = restaurant_list.results.find((restaurant) => restaurant.id.toString() === req.params.restaurant.toString())
-  res.render('show', {restaurant : restaurant_id})
+  const id = req.params.restaurant
+  Restaurant_list.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', {restaurant}))
+    .catch(error => console.error(error))
+  
 })
 
 //搜尋餐廳
 app.get('/search', (req, res) => {
-  category_restaurant = sortAndPick(pre_category_restaurant)
+  //category_restaurant = sortAndPick(pre_category_restaurant)
   const keyword = (req.query.keyword).replace(/\s*/g,"")
 
   //一般keywords搜尋、keywords分類搜尋==>顯示搜尋資料
-  const restaurants = restaurant_list.results.filter((restaurant) =>{
+  const restaurants = Restaurant_list.find((restaurant) => {
     //搜尋到幾筆相關資料(搜尋後才產生)
     search_result_howmany_restaurants = true
     //temp_restaurants 暫放搜尋資料
     let temp_restaurants = restaurant.name.toLowerCase().includes(keyword.toLowerCase())
     temp_restaurants += restaurant.category.includes(keyword)
     return temp_restaurants
-  })
+    console.log(restaurant.name)
+
+  }) 
+  console.log(restaurants)
 
   //特殊keywords搜尋(未輸入)==>顯示全部資料
   if(keyword === ''){
     search_result_howmany_restaurants = false
-  }
+  } 
   res.render('index', {restaurants : restaurants, keyword : keyword,search_result : search_result_howmany_restaurants, category : category_restaurant})
 })
 
@@ -80,7 +91,8 @@ app.listen(port, () => {
 //隨機抽取4個
 function sortAndPick(pre_category_restaurant){
   //search category
-  restaurant_list.results.forEach((restaurant) => {
+
+  Restaurant_list.forEach((restaurant) => {
     pre_category_restaurant.add(restaurant.category)
   })
 
