340133bae6094e9ae6dc1cde2a79201ea6616ecb
asterchang96@gmail.com <asterchang96@gmail.com>
2021-08-30 15:42:20 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -55,10 +56,15 @@ router.post('/register', (req, res) => {
         })
       }
       //success
-      return User.create({
-        name,
-        email,
-        password
+      return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash =>{
+        User.create({
+          name,
+          email,
+          password: hash
+        })
       })
       .then(() => res.redirect('/'))
       .catch(err => console.error(err))
